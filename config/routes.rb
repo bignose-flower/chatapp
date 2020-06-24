f77baa8418e8ca011e2f73838420df6ec6b4fa17
bignose-flower <66852865+bignose-flower@users.noreply.github.com>
2020-06-24 17:42:01 +0900

    create controller for message send a message

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
 
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:index, :new, :create, :edit, :update]
-    resources :messages, only: [:index, :new, :create]
+  resources :groups, only: [:index, :new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
