fbd1c44ac3d1e6f673fbf37caf2e498018d1ab83
bignose-flower <66852865+bignose-flower@users.noreply.github.com>
2020-06-25 13:03:31 +0900

    change response method from html to json

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,11 +9,17 @@ class MessagesController < ApplicationController
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-      redirect_to group_messages_path(@group)
+      respond_to do |format|
+        format.json
+      end
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください'
-      render :index
+      respond_to do |format|
+        format.html { render :view }
+        format.json
+      end
+        
     end
   end
 
