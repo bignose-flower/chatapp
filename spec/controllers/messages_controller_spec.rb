a9dc8b418c705725023b0bf5a0d2facf7355ea17
bignose-flower <66852865+bignose-flower@users.noreply.github.com>
2020-06-25 11:23:06 +0900

    create message model and #index action of controller

---
+++ spec/controllers/messages_controller_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+describe MessagesController do
+  let(:group) { create(:group) }
+  let(:user) { create(:user) }
+
+  describe '#index' do
+    context 'log in' do
+      before do
+        login user
+        get :index, params: { group_id: group.id }
+      end
+
+      it 'assigns group' do
+        expect(assigns(:group)).to eq group
+      end
+
+      it 'assigns message' do
+        expect(assigns(:message)).to be_a_new(Message)
+      end
+
+      it 'renders index view' do
+        expect(response).to render_template :index
+      end
+    end
+
+    context 'not log in' do
+      before do
+        get :index, params: { group_id: group.id }
+      end
+
+      it 'redirects to index login view' do
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
+  describe '#create' do
+    context 'log in' do
+      context 'success to save' do
+        
+      end
+
+      context 'failure to save' do
+    end
+
+    context 'not log in' do
+      
+    end
+  end
+end
--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :message do
-    content {Faker::Lorem.name}
+    content {Faker::Lorem.sentence}
     image {File.open("#{Rails.root}/public/images/test_image.jpg")}
     group
     user
