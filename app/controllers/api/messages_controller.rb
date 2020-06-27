8ee61706500ae03fb3bc0da8d9d1e50afebb3455
bignose-flower <66852865+bignose-flower@users.noreply.github.com>
2020-06-27 19:52:39 +0900

    change api controller

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -2,6 +2,9 @@ class Api::MessagesController < ApplicationController
   def index
     group = Group.find(params[:group_id])
     last_message_id = params[:id]
-    @messages = group.messages.includes(:user).where("id > ?", last_message_id)
+    if ( last_message_id? )
+      return @messages = group.messages.includes(:user).where("id > ?", last_message_id)
+    end
+    last_message_id = 0
   end
 end
